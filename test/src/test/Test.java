eb0526c868b8e997c98c00b204d9add14af4ad84
hw5711 <huanwu5711@gmail.com>
2019-02-21 15:30:13 -0600

    finished

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		
 		// Create 11 jobs in wait queue
 		int m = 11;
-		System.out.println("Display original wait queue(priority unsorted).");
+		System.out.println("\nStep a: Display original wait queue(priority unsorted).");
 		System.out.println("Job#   "+"Job Name"+"  Submitter"+"   priority");
 		WaitQueue wQ = new WaitQueue(m);
 		for(int i = 0; i < m; i++) {
@@ -21,7 +21,7 @@ public class Test {
 		
 		// Step b: sort wait queue and display
 		wQ.heapSort();
-		System.out.println("\nDisplay sorted wait queue( by priority).");
+		System.out.println("\nStep b: Display sorted wait queue( by priority).");
 		wQ.display();
 		
 		// Step c: move 4 times jobs into ready queue
@@ -31,16 +31,19 @@ public class Test {
 		rQ.insert(wQ.getWaitQ().get(0));
 		wQ.remove();
 		}
-		System.out.println("\nAfter 4 times deletion(wait Q) and addition(ready Q)");
-		System.out.println("Wait Queue:");
+		System.out.println("\nStep c: 4 times deletion(wait Q) and addition(ready Q)");
+		System.out.println("\nWait Queue:");
 		wQ.display();
 		System.out.println("\nReady Queue:");
 		rQ.display();
 		
 		// Step d: delete 2 jobs from ready queue and display
+		System.out.println("\nStep d: Removing 2 jobs form ready queue");
 		rQ.remove();
 		rQ.remove();
-		System.out.println("\nAfter removing 2 jobs form ready queue");
+		System.out.println("\nWait Queue(no changes in this step):");
+		wQ.display();
+		System.out.println("\nReady Queue:");
 		rQ.display();
 		
 		
@@ -49,7 +52,9 @@ public class Test {
 			String tempStr1 = "JobName2"+ Integer.toString(i);
 			String tempStr2 = "Submitter2"+Integer.toString(i);
 			int tempNum = 0 +  (int)(Math.random() * m + 1);
+			wQ.setWaitQsize(wQ.getWaitQsize()+1);
 			wQ.getWaitQ().add(new Job(200+i, tempStr1, tempStr2, tempNum));
+			wQ.heapSort();
 		}
 		// Move 3 jobs from wait queue to ready queue
 		for(int i = 0; i< 3; i++) {
@@ -59,11 +64,36 @@ public class Test {
 		// Delete 1 job from ready queue
 		rQ.remove();
 		// Display
-		System.out.println("\nAfter step e");
-		System.out.println("Wait Queue:");
+		System.out.println("\nStep e");
+		System.out.println("Wait Queue(add 2 jobs and remove 3 jobs):");
+		
 		wQ.display();
-		System.out.println("\nReady Queue:");
+		System.out.println("\nReady Queue(add 3 job from wait queue and remove 1 job):");
 		rQ.display();
+		
+		// Step f: Change 2 priority jobs in the wait queue
+		wQ.getWaitQ().get(0).setPriority(1);
+		wQ.getWaitQ().get(1).setPriority(1);
+		System.out.println("\nStep f");
+		System.out.println("\nWait Queue(change first two jobs' priority to 1):");
+		wQ.heapSort();
+		wQ.display();
+		
+		// Step g: remove all rest jobs , display
+		while(wQ.getWaitQsize() != 0) {
+			rQ.insert(wQ.getWaitQ().get(0));
+			wQ.remove();
+		}
+		
+		while(rQ.getReadyQsize() != 0) {
+			rQ.remove();
+		}
+		System.out.println("\nStep f");
+		System.out.println("\nWait Queue(empty):");
+		wQ.display();
+		System.out.println("\nReady Queue(empty):");
+		rQ.display();
+		
 		
         
         
