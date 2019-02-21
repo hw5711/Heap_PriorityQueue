a80b778bac7fbd0840ef397c768dc0aa604d5d61
hw5711 <huanwu5711@gmail.com>
2019-02-21 14:12:56 -0600

    sorted waitQ

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -7,8 +7,8 @@ public class Test {
 		
 		// Create 11 jobs in wait queue
 		int m = 11;
-		System.out.println("Display original wait queue(priority unsorted.");
-		System.out.println("Job#   "+"Job Name"+"  Submitter"+" priority");
+		System.out.println("Display original wait queue(priority unsorted).");
+		System.out.println("Job#   "+"Job Name"+"  Submitter"+"   priority");
 		WaitQueue wQ = new WaitQueue(m);
 		for(int i = 0; i < m; i++) {
 			String tempStr1 = "JobName"+ Integer.toString(i);
@@ -16,8 +16,12 @@ public class Test {
 			int tempNum = 0 +  (int)(Math.random() * m + 1);
 			wQ.getWaitQ().add(new Job(100+i, tempStr1, tempStr2, tempNum));
 		}
-		
 		wQ.display();
+		wQ.heapSort();
+		System.out.println("\nDisplay sorted wait queue( by priority).");
+		wQ.display();
+		
+		
 
         
         
--- test/src/test/WaitQueue.java
+++ test/src/test/WaitQueue.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class WaitQueue {
 	int waitQsize;
@@ -13,7 +14,7 @@ public class WaitQueue {
 	}
 
 	public int getWaitQsize() {
-		return waitQsize;
+		return this.waitQsize;
 	}
 
 	public void setWaitQsize(int waitQsize) {
@@ -21,7 +22,7 @@ public class WaitQueue {
 	}
 
 	public ArrayList<Job> getWaitQ() {
-		return waitQ;
+		return this.waitQ;
 	}
 
 	public void setWaitQ(ArrayList<Job> waitQ) {
@@ -31,10 +32,55 @@ public class WaitQueue {
 	public void display() {
 
 		for (int i = 0; i < this.waitQsize; i++) {
-			System.out.println(this.waitQ.get(i).getJobNum() + " "
-					+ this.waitQ.get(i).getJobName() + " "
-					+ this.waitQ.get(i).getSubmitter()+" "+this.waitQ.get(i).getPriority());
+			System.out.println(this.waitQ.get(i).getJobNum() + "   "
+					+ this.waitQ.get(i).getJobName() + "   "
+					+ this.waitQ.get(i).getSubmitter() + "     "
+					+ this.waitQ.get(i).getPriority());
 		}
+	}
+
+	//heap sort
+	public static void buildheap(ArrayList<Job> waitQ) {
+
+		for (int i = (waitQ.size() - 1) / 2; i >= 0; i--) {
+			heapify(waitQ, i, waitQ.size() - 1);
+		}
+	}
+
+	public static void heapify(ArrayList<Job> waitQ, int i, int size) {
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+		int max;
+		if (left <= size && waitQ.get(left).getPriority() < waitQ.get(i).getPriority()) {
+			max = left;
+		} else {
+			max = i;
+		}
+
+		if (right <= size && waitQ.get(right).getPriority() < waitQ.get(max).getPriority()) {
+			max = right;
+		}
+		// If max is not current node, exchange it with max of left and right
+		// child
+		if (max != i) {
+			Collections.swap(waitQ, i, max);
+			heapify(waitQ, max, size);
+		}
+	}
+
+
+	public ArrayList<Job> heapSort() {
+
+		buildheap(this.waitQ);
+		int sizeOfHeap = this.waitQ.size() - 1;
+		for (int i = sizeOfHeap; i > 0; i--) {
+			Collections.swap(this.waitQ, 0, i);
+			sizeOfHeap = sizeOfHeap - 1;
+			heapify(this.waitQ, 0, sizeOfHeap);
+		}
+		return this.waitQ;
+		//this.setWaitQ(this.waitQ);
+		
 	}
 
 }
