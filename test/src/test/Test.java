55304712e470dd3ab73c43ac30de011b771edf9d
hw5711 <huanwu5711@gmail.com>
2019-02-21 13:50:06 -0600

    add wait queue content

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,7 +1,5 @@
 package test;
 
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Test {
 	
@@ -11,14 +9,15 @@ public class Test {
 		int m = 11;
 		System.out.println("Display original wait queue(priority unsorted.");
 		System.out.println("Job#   "+"Job Name"+"  Submitter"+" priority");
+		WaitQueue wQ = new WaitQueue(m);
 		for(int i = 0; i < m; i++) {
 			String tempStr1 = "JobName"+ Integer.toString(i);
 			String tempStr2 = "Submitter"+Integer.toString(i);
 			int tempNum = 0 +  (int)(Math.random() * m + 1);
-			ArrayList<Job> rQ = new ArrayList<Job>();
-			rQ.add(new Job(100+i, tempStr1, tempStr2, tempNum));
-			
+			wQ.getWaitQ().add(new Job(100+i, tempStr1, tempStr2, tempNum));
 		}
+		
+		wQ.display();
 
         
         
--- test/src/test/WaitQueue.java
+++ test/src/test/WaitQueue.java
@@ -4,12 +4,37 @@ import java.util.ArrayList;
 
 public class WaitQueue {
 	int waitQsize;
-	ArrayList<Job> waitQ = new ArrayList<Job>();
-	
-	public WaitQueue(int waitQsize, ArrayList<Job> waitQ) {
+	ArrayList<Job> waitQ;// = new ArrayList<Job>();
+
+	public WaitQueue(int waitQsize) {
 		super();
 		this.waitQsize = waitQsize;
+		this.waitQ = new ArrayList<Job>();
+	}
+
+	public int getWaitQsize() {
+		return waitQsize;
+	}
+
+	public void setWaitQsize(int waitQsize) {
+		this.waitQsize = waitQsize;
+	}
+
+	public ArrayList<Job> getWaitQ() {
+		return waitQ;
+	}
+
+	public void setWaitQ(ArrayList<Job> waitQ) {
 		this.waitQ = waitQ;
+	}
+
+	public void display() {
+
+		for (int i = 0; i < this.waitQsize; i++) {
+			System.out.println(this.waitQ.get(i).getJobNum() + " "
+					+ this.waitQ.get(i).getJobName() + " "
+					+ this.waitQ.get(i).getSubmitter()+" "+this.waitQ.get(i).getPriority());
+		}
 	}
 
 }
